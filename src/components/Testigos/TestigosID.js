12608732263fcde6e570ca08982c1705703f414e
jesus <jesus-262@hotmail.com>
2022-02-05 23:52:18 -0500

    arreglo departamentos y minicipios en testigosid

--- src/components/Testigos/TestigosID.js
+++ src/components/Testigos/TestigosID.js
@@ -244,8 +244,7 @@ rendermunicipio= () => {
   }
   async componentDidMount() {
 
-    this.mostrardepartamentos();
-    this.unmunicipio();
+   
     var parametro=this.props.match.params.id;
     console.log(parametro);
     if(this.esBASE64(parametro)){
@@ -418,7 +417,7 @@ class="inputfile inputfile-5"
 data-multiple-caption="{count} archivos seleccionados" multiple />
 <label for="file-5">
 <figure>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
+<svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
   <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
   <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
 </svg>
